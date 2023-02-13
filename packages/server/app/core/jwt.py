2ffdf32864eca3a9ff3b8843db1c671cea53445e
alex <alexs123655@gmail.com>
2023-02-13 12:13:56 -0300

    feat: api core

---
+++ packages/server/app/core/jwt.py
@@ -0,0 +1,18 @@
+from datetime import datetime, timedelta
+from typing import Union, Any
+
+from jose import jwt
+from app.core.settings import ALGORITHM, ACCESS_TOKEN_EXPIRE_MINUTES, SECRET_KEY
+
+
+def createAccessToken(
+    *, data: dict, subject: Union[str, Any], expires_delta: timedelta = None
+):
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.utcnow() + expires_delta
+    else:
+        expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    to_encode.update({"exp": expire, "sub": subject})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
---
+++ packages/server/app/core/settings.py
@@ -0,0 +1,10 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+SQLALCHEMY_DATABASE_URL = os.environ.get("DATABASE_URL")
+SECRET_KEY = "39e9ee9b41bcb2e444d4086f12a50d2f"  # must be in env
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 30  # 30 minutes
+REFRESH_TOKEN_EXPIRE_MINUTES = 60 * 24 * 7  # 7 days
