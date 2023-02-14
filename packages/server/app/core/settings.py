113c12ea6b313e6c091cc7573c91f45fa1d266b9
alex <alexs123655@gmail.com>
2023-02-13 22:18:20 -0300

    feat: add new environment variables

--- packages/server/app/core/settings.py
+++ packages/server/app/core/settings.py
@@ -8,3 +8,6 @@ SECRET_KEY = "39e9ee9b41bcb2e444d4086f12a50d2f"  # must be in env
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 30  # 30 minutes
 REFRESH_TOKEN_EXPIRE_MINUTES = 60 * 24 * 7  # 7 days
+
+DEFAULT_USER = "staff@admin.com"
+DEFAULT_PASSWORD = "hardpassword"
